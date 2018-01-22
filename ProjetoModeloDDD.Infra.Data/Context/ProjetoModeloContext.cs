4bda9877f6d079049f7a3f8c68ca3b88f762967d
davtrig <david.ordaz96@gmail.com>
2018-01-22 11:36:26 +0000

    Stuff

--- ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetoModeloDDD.Infra.Data.Context
 {
-    public class ProjetoModeloContext :DbContext
+    public class ProjetoModeloContext : DbContext
     {
+        public ProjetoModeloContext()
+            : base("ProjetoModeloDDD")
+        {
 
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
