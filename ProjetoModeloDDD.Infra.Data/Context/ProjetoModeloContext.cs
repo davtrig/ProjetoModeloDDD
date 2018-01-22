2e392e31e92b0be422ac4941c8ec57f39eb9aa23
David Trigueiros <david.ordaz96@gmail.com>
2018-01-22 13:55:47 +0000

    SaveChanges

--- ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -25,6 +25,31 @@ namespace ProjetoModeloDDD.Infra.Data.Context
             modelBuilder.Properties()
                 .Where(p => p.Name == p.ReflectedType.Name + "Id")
                 .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries()
+                .Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
         }
     }
 }
